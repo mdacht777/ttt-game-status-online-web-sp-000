63298187c59a5ea049469e674a356354ba7f4a2b
Martin Achtermann <mdacht777@gmail.com>
2020-05-29 22:09:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ WIN_COMBINATIONS.each do |win_combination|
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combination # return the win_combination indexes that won.
   else
-    #return false
+    return false
   end
 end
 end
