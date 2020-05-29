c25504022451ceb883fbedc98d3089249727c1c9
Martin Achtermann <mdacht777@gmail.com>
2020-05-29 22:10:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ WIN_COMBINATIONS.each do |win_combination|
   position_2 = board[win_index_2] # load the value of the board at win_index_2
   position_3 = board[win_index_3] # load the value of the board at win_index_3
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    win_combination # return the win_combination indexes that won.
+    return win_combination # return the win_combination indexes that won.
   else
     return false
   end
